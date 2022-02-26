4fdec211e6d7e208df292c6813fa78fd8f5b7ff4
yjcho <zzoyu@hotmail.com>
2022-02-26 16:42:07 +0900

    build(eslint): change rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,9 +12,7 @@ module.exports = {
   plugins: [],
   // add your custom rules here
   rules: {
-    'vue/multi-word-component-names': 0,
-    'no-console': 'off',
-    'no-empty-source': null
+    'no-console': 0,
   },
-  
+
 }
