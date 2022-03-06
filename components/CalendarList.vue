adde1a6aa26c5a2fc042c8aad0f5ce9201e9fc79
zzoyu <zzoyu@hotmail.com>
2022-03-06 23:02:17 +0900

    feat(calendar): add calendar inner list

---
+++ components/CalendarList.vue
@@ -0,0 +1,26 @@
+<template>
+  <ul>
+    <li v-for="(record, i) in listRecord" :key="i">{{ record.title }}</li>
+  </ul>
+</template>
+
+<script>
+import { recordsGetByDate } from '@/utils/axios'
+import { getSession } from '~/utils/auth'
+export default {
+  props: {
+    date: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      listRecord: [],
+    }
+  },
+  async fetch() {
+    this.listRecord = await recordsGetByDate(getSession(), this.date)
+  },
+}
+</script>
